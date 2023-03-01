6025db664383da6acd6fce6f68fd99f05d69aaef
Monine Chan <mochan@students.42wolfsburg.de>
2023-03-01 21:55:06 +0100

    add additional messages above the current level

--- ex06/src/Harl.cpp
+++ ex06/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 22:05:18 by mochan            #+#    #+#             */
-/*   Updated: 2023/03/01 21:29:49 by mochan           ###   ########.fr       */
+/*   Updated: 2023/03/01 21:48:59 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,38 @@ Harl::~Harl()
 void	Harl::debug( void )
 {
 	std::cout << GREEN <<"[ DEBUG ]" << D << "\n";
-	std::cout << GREEN <<"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << D << "\n";
+	std::cout << GREEN <<"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << D << "\n\n";
 }
 
 void	Harl::info( void )
 {
 	std::cout << BLU << "[ INFO ]" << D << "\n";
-	std::cout << BLU << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << D << "\n";
+	std::cout << BLU << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << D << "\n\n";
 }
 
 void	Harl::warning( void )
 {
 	std::cout << YELL << "[ WARNING ]" << D << "\n";
-	std::cout << YELL << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << D << "\n";
+	std::cout << YELL << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << D << "\n\n";
 }
 
 void	Harl::error( void )
 {
 	std::cout << RED << "[ ERROR ]" << D << "\n";
-	std::cout << RED << "This is unacceptable! I want to speak to the manager now." << D << "\n";
+	std::cout << RED << "This is unacceptable! I want to speak to the manager now." << D << "\n\n";
 }
 
 void	Harl::complain (std::string level)
 {
-	std::string	funct[4] = {"debug", "info", "warning", "error"}; // Array of the name of member functions
+	void		(Harl::*FuncPtr[4])(void); // Declaration of an array of Pointers to member functions
+	std::string	funct[4] = {"DEBUG", "INFO", "WARNING", "ERROR"}; // Array of the name of member functions
 	int			i;
 	
+	//initialization of Pointers to member functions
+	FuncPtr[0] = &Harl::debug; 
+	FuncPtr[1] = &Harl::info;
+	FuncPtr[2] = &Harl::warning;
+	FuncPtr[3] = &Harl::error;
 	i = 1;
 	while (i < 5)
 	{
@@ -61,19 +67,35 @@ void	Harl::complain (std::string level)
 	switch(i)
 	{
 		case 1:
-			debug();
+			while ( i - 1 < 4)
+			{
+				(this->*FuncPtr[i - 1])();
+				i++;
+			}
 			break;
 		case 2:
-			info();
+			while ( i - 1 < 4)
+			{
+				(this->*FuncPtr[i - 1])();
+				i++;
+			}
 			break;
 		case 3:
-			warning();
+			while ( i - 1 < 4)
+			{
+				(this->*FuncPtr[i - 1])();
+				i++;
+			}
 			break;
 		case 4:
-			error();
+			while ( i - 1 < 4)
+			{
+				(this->*FuncPtr[i - 1])();
+				i++;
+			}
 			break;
 		default:
-			std::cout << "[ WRONG COMMAND ]" << "\n";
+			std::cout << RED << "/!\\ WRONG COMMAND /!\\" << D << "\n";
 			break;
 	}
 }
