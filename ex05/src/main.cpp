1dda513d1041615da03b868d9c63a33554d8d687
Monine Chan <mochan@students.42wolfsburg.de>
2023-03-01 21:54:19 +0100

    modify arguments for complain function in uppercase, change aspect of cout messages

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mochan <mochan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 22:04:29 by mochan            #+#    #+#             */
-/*   Updated: 2023/03/01 18:53:27 by mochan           ###   ########.fr       */
+/*   Updated: 2023/03/01 21:51:21 by mochan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	main()
 {
 	Harl harl;
 
-	harl.complain("debug");
-	harl.complain("info");
-	harl.complain("warning");
-	harl.complain("error");
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
 	return (0);
 }
